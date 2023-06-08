28294821cb701cd2e7cff8a8554cff45d59bd6f6
vladimirdevico1 <info@vladimirdevico.com>
2023-06-09 00:42:14 +0200

    feat(hooks): add useInView hook

---
+++ Frontend/src/hooks/useInView.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useInView() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setInView(entry.isIntersecting);
+
+      // Adjust this value to determine how much of the component needs to be visible before loading
+    }, { threshold: 0.1 });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  return [ref, inView] as const;
+}
+
+export default useInView;
